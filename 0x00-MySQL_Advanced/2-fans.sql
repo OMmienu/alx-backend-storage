72e4e71f8b9029eb26fa8e8e34af839c3ab75151
OMmienu <nkedzi@gmail.com>
2023-07-13 17:18:56 -0700

    2-fans.sql error fixed

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
---SQL Script that ranks country of origin of bands , ordered by number of
---- (non-unique) fans
-SELECT origin, SUM(fans) as nb_fans
+-- SQL script that ranks country of origins of bands, ordered by number of
+-- (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans 
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
