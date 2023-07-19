a10a78ec665a7cc5c1e4f396422c2c798bb7bab3
OMmienu <nkedzi@gmail.com>
2023-07-19 23:15:33 +0000

    web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""web cache and tracker """
+
+from functools import wraps
+from typing import Callable
+
+import redis
+import requests
+from requests import Response
+
+_redis = redis.Redis()
+_redis.flushdb()
+
+
+def counter(method: Callable) -> Callable:
+    """
+    a counter decorator that counts how many times a particular URL was
+    accessed. The value is cached in Redis and will expire after 10 seconds
+    """
+
+    @wraps(method)
+    def wrapper(url):
+        """
+        wrapper function
+        """
+        _redis.incr(f"count:{url}")
+
+        html = _redis.get(f"cached:{url}")
+        if html is not None:
+            return html.decode("utf-8")
+        html = method(url)
+        _redis.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@counter
+def get_page(url: str) -> str:
+    """
+    a function that returns the HTML content of a particular URL
+    """
+    response: Response = requests.get(url)
+    return response.text
